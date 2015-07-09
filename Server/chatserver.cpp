b41bcb9202e00b28ed789f72f07bed2fdc67d230
LisGein <alred402@gmail.com>
2015-07-09 03:41:28 +0300

    Добавила типы сообщений

--- Server/chatserver.cpp
+++ Server/chatserver.cpp
@@ -8,57 +8,51 @@ ChatServer::ChatServer(int port, QObject *parent)
     stringServer_ = new StringServer(port);
     connect(stringServer_, SIGNAL(ready_msg(QByteArray, QHostAddress, quint16)), this, SLOT(read_in_data(QByteArray, QHostAddress, quint16)));
     connect(stringServer_, SIGNAL(delete_user(QHostAddress, quint16)), this, SLOT(delete_user(QHostAddress, quint16)));
+    message_processors_map_["msg"] = std::bind(&ChatServer::process_msg, this, std::placeholders::_4);
+    message_processors_map_["register"] = std::bind(&ChatServer::reg_user, this, std::placeholders::_1, std::placeholders::_2,std::placeholders:: _3);
+    message_processors_map_["private"] = std::bind(&ChatServer::private_msg, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3,  std::placeholders::_4);
+    message_processors_map_["online"] = std::bind(&ChatServer::list_online, this, std::placeholders::_2, std::placeholders::_3);
 }
 
 ChatServer::~ChatServer()
 {
 }
 
-void ChatServer::delete_user(const QHostAddress &host_sender, quint16 port_sender)
+void ChatServer::list_online(QHostAddress const& host_sender, quint16 port_sender)
 {
-    auto it = users_.find(port_sender);
-    QString msg_text = it.value();
-    msg_text.append(" disconnected");
-    QByteArray return_message = create_msg(msg_text);
+    rapidjson::Document d;
+    d.SetObject();
+    rapidjson::Document::AllocatorType& allocator = d.GetAllocator();
+    d.AddMember("type", "online_users", allocator);
+    rapidjson::Value msg_key;
+    msg_key.SetArray();
+    for (auto it = users_.begin(); it != users_.end(); ++it)
+    {
+        rapidjson::Value msg_value;
+        msg_value.SetString(it.value().toUtf8().constData(), it.value().toUtf8().size(), allocator);
+        msg_key.AddMember("name", msg_value, allocator);
 
-    stringServer_->send_all(return_message);
-    stringServer_->send_all("\n");
+        qDebug() << msg_key["name"].GetString();
+    }
+    d.AddMember("list", msg_key, allocator);
+    qDebug() << "e";
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer, rapidjson::Document::EncodingType, rapidjson::ASCII<> > writer(buffer);
+    d.Accept(writer);
 
-    re_users_.remove(it.value());
-    users_.remove(it.key());
+    stringServer_->send_private(port_sender, buffer.GetString());
+    stringServer_->send_private(port_sender, "\n");
 }
 
-void ChatServer::read_in_data(QByteArray message, const QHostAddress &host_sender, quint16 port_sender)
+void ChatServer::process_msg(QByteArray message)
 {
-    rapidjson::Document doc;
-    doc.Parse(message);
-    bool fail_parse = doc.HasParseError();
-    auto it = users_.find(port_sender);
-    QString text_message = doc["msg"].GetString();
-
-    if (fail_parse)
-        error_parse_msg();
-
-    else if (it != users_.end())
-        if (text_message[0] == '@')
-            send_private_msg(message, text_message, host_sender, port_sender);
-        else
-            stringServer_->send_all(message);
-
-    else
-        connect_new_user(text_message, host_sender, port_sender);
-
+    stringServer_->send_all(message);
 }
 
-void ChatServer::error_parse_msg()
+void ChatServer::private_msg(rapidjson::Document const & doc, QHostAddress const& host_sender, quint16 port_sender, QByteArray message)
 {
-    qDebug() << "error parse message";
-}
-
-void ChatServer::send_private_msg(QByteArray const& message, QString const& msg, const QHostAddress &host_sender, quint16 port_sender)
-{
-    QString nick = find_receiver_nick( msg);
-    auto i = re_users_.find(nick);
+    QString addressee = doc["addressee"].GetString();
+    auto i = re_users_.find(addressee);
     if (i != re_users_.end())
     {
         stringServer_->send_private(i.value(), message);
@@ -66,12 +60,60 @@ void ChatServer::send_private_msg(QByteArray const& message, QString const& msg,
     }
     else
     {
-        QString msg_text = nick;
+        QString msg_text = addressee;
         msg_text .append(" offline");
         QByteArray return_message = create_msg(msg_text);
         stringServer_->send_private(port_sender, return_message);
         stringServer_->send_private(port_sender, "\n");
     }
+}
+
+void ChatServer::reg_user(rapidjson::Document const & doc, QHostAddress const& host_sender, quint16 port_sender)
+{
+    QString text_message = doc["msg"].GetString();
+    re_users_.insert(text_message, port_sender);
+    users_.insert(port_sender, text_message);
+    text_message.append(" connected");
+    QByteArray return_message = create_msg(text_message);
+    stringServer_->send_all(return_message);
+    stringServer_->send_all("\n");
+
+}
+
+void ChatServer::delete_user(const QHostAddress &host_sender, quint16 port_sender)
+{
+    auto it = users_.find(port_sender);
+    if (it != users_.end())
+    {
+        QString msg_text = it.value();
+        msg_text.append(" disconnected");
+        QByteArray return_message = create_msg(msg_text);
+
+        stringServer_->send_all(return_message);
+        stringServer_->send_all("\n");
+
+        re_users_.remove(it.value());
+        users_.remove(it.key());
+    }
+}
+
+void ChatServer::read_in_data(QByteArray message, const QHostAddress &host_sender, quint16 port_sender)
+{
+    rapidjson::Document doc;
+    doc.Parse(message);
+    bool fail_parse = doc.HasParseError();
+    QString type = doc["type"].GetString();
+    if (fail_parse)
+        error_parse_msg();
+    else if (message_processors_map_.contains(type))
+        message_processors_map_[type](doc, host_sender, port_sender, message);
+    else
+        error_parse_msg();
+}
+
+void ChatServer::error_parse_msg()
+{
+    qDebug() << "error parse message";
 }
 
 QByteArray ChatServer::create_msg(QString const& msg)
@@ -88,24 +130,5 @@ QByteArray ChatServer::create_msg(QString const& msg)
     d.Accept(writer);
 
     return buffer.GetString();
-}
-
-void ChatServer::connect_new_user(QString text_message, QHostAddress const& host_sender, quint16 port_sender)
-{
-    re_users_.insert(text_message, port_sender);
-    users_.insert(port_sender, text_message);
-    text_message.append(" connected");
-    QByteArray return_message = create_msg(text_message);
-    stringServer_->send_all(return_message);
-    stringServer_->send_all("\n");
-}
-
-QString ChatServer::find_receiver_nick(QString const& msg)
-{
-    int pos = msg.indexOf(' ');
-    QString nick= msg;
-    nick.remove(0,1);
-    nick.resize(pos-1);
-    return nick;
 }
 
--- Server/chatserver.h
+++ Server/chatserver.h
@@ -2,30 +2,34 @@
 #include <QTcpServer>
 #include <QTcpSocket>
 #include "stringserver.h"
+#include <functional>
 
+typedef std::function<void(rapidjson::Document const &, QHostAddress const&, quint16, QByteArray)> message_processor_t;
 
 class ChatServer : public QObject
 {
-   Q_OBJECT
+    Q_OBJECT
 
 public:
-   explicit ChatServer(int port, QObject *parent = 0);
-   ~ChatServer();
+    explicit ChatServer(int port, QObject *parent = 0);
+    ~ChatServer();
 
 private slots:
-   void read_in_data(QByteArray message, QHostAddress const& host_sender, quint16 port_sender);
-   void delete_user(QHostAddress const& host_sender, quint16 port_sender);
+    void read_in_data(QByteArray message, QHostAddress const& host_sender, quint16 port_sender);
+    void delete_user(QHostAddress const& host_sender, quint16 port_sender);
 
 private:
-   QTcpServer* tcp_server_;
-   StringServer *stringServer_;
-   QMap< quint16, QString> users_;
-   QMap< QString, quint16> re_users_;
+    QTcpServer* tcp_server_;
+    StringServer *stringServer_;
+    QMap< quint16, QString> users_;
+    QMap< QString, quint16> re_users_;
+    QMap<QString, message_processor_t> message_processors_map_;
 
-   void send_data(QTcpSocket *tcp_socket, QString const& message);
-   QString find_receiver_nick(QString const& msg);
-   void error_parse_msg();
-   QByteArray create_msg(QString const& msg);
-   void send_private_msg(QByteArray const& message, QString const& msg, QHostAddress const& host_sender, quint16 port_sender);
-   void connect_new_user(QString text_message, QHostAddress const& host_sender, quint16 port_sender);
+    void send_data(QTcpSocket *tcp_socket, QString const& message);
+    void error_parse_msg();
+    void process_msg(QByteArray message);
+    void private_msg(rapidjson::Document const & doc, QHostAddress const& host_sender, quint16 port_sender, QByteArray message);
+    void reg_user(rapidjson::Document const & doc, QHostAddress const& host_sender, quint16 port_sender);
+    QByteArray create_msg(QString const& msg);
+    void list_online(QHostAddress const& host_sender, quint16 port_sender);
 };
