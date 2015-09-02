3f9192ff2dcc859f9e55ea4fafc1ad801511973b
LisGein <alred402@gmail.com>
2015-09-02 22:59:46 +0300

    Считывание аргументов командной строки, перевод на python3

--- Python/learn/weather.py
+++ Python/learn/weather.py
@@ -1,23 +1,27 @@
 # coding: utf8
 import xml.etree.ElementTree as ET
 import urllib
+import argparse
 
-country = str(raw_input('Input county: ')).decode('utf-8')
-city = str(raw_input('Input city: ')).decode('utf-8')
+parser = argparse.ArgumentParser()
+parser.add_argument('country', type=str, help='country help')
+parser.add_argument('city', type=str, help='city help')
+args = parser.parse_args()
+
 tree = ET.parse(urllib.urlopen('https://pogoda.yandex.ru/static/cities.xml'))
 
 root_tree = tree.getroot()
 idInputCity = 'http://export.yandex.ru/weather-ng/forecasts/'
-for countries in root_tree.findall('country', tree):
-    if (countries.get('name') == country):
-        for cities in countries.findall('city', tree):
-           if (cities.text == city):
+for countries in root_tree.findall('country'):
+    if (countries.get('name') == args.country.decode('utf-8')):
+        for cities in countries.findall('city'):
+           if (cities.text == args.city.decode('utf-8')):
                 idInputCity += cities.get('id')
 
 idInputCity += '.xml'
 
 weather = ET.parse(urllib.urlopen(idInputCity))
 root_weather = weather.getroot()
-for fact in root_weather.findall('{http://weather.yandex.ru/forecast}fact', weather):
-    for child in fact.findall('{http://weather.yandex.ru/forecast}temperature', weather):
-      print 'temperature =',child.text
+for fact in root_weather.findall('{http://weather.yandex.ru/forecast}fact'):
+    for child in fact.findall('{http://weather.yandex.ru/forecast}temperature'):
+      print('temperature = '+child.text)
