a2f6f354ab699321a6699360c857f225236d71cf
LisGein <alred402@gmail.com>
2015-10-02 17:00:36 +0300

    В цикле ошибка

--- Python/nightYandex/main.py
+++ Python/nightYandex/main.py
@@ -1,33 +1,78 @@
-import matplotlib.pyplot as plt
 import pandas
-from sklearn.cluster import KMeans
+import math
 
-data = pandas.read_csv('data.tsv', sep="\t", header=None, names=['date', 'id', 'type', 'hash', 'latitude', 'longitude'])
+data = pandas.read_csv('data1.tsv', sep="\t")
+route_to_stops_count = pandas.read_csv('route1.tsv', sep="\t")
+debug = pandas.read_csv('debug1.tsv', sep="\t")
+# names=['date', 'id', 'type', 'hash', 'latitude', 'longitude']
 
-for id in data["id"].unique():
-    t = data[data["id"] == id]
-    plt.plot(t['latitude'], t['longitude'])
-a = 0
-print("q")
-for id in data["id"].unique():
-    a += len(data[data["id"] == id])
-    print((data[data["id"] == id]).unique())
-print(len(a))
-'''route_to_stops_count = pandas.read_csv('route_to_stops_count.tsv', sep="\t", header=None, names=['hash', 'num'])
-x = route_to_stops_count[route_to_stops_count["hash"] == 86]
-k_means = KMeans(n_clusters = x["num"][0])
-k_means.fit(data[['latitude', 'longitude']])
 
-debug = pandas.read_csv('debug.tsv', sep="\t", header=None, names=['type', 'hash', 'latitude', 'longitude'])
-t = debug[debug["hash"] == 86]
-z = t[debug["type"] == "bus"]
-plt.plot(z['latitude'], z['longitude'], 'o')
-a = []
-for i in range(x["num"][0]):
-    a.append(k_means.cluster_centers_[i])
+X = debug["latitude"].copy()
+Y = debug["longitude"].copy()
 
-plt.plot(a[:,0], a[:,1], 'ro')
+#X = [1, 3, 6, 8, 20]
+#Y = [0, 4, 6, 7, 10]
+eps = 0
+for i in range(len(X) - 1):
+    eps = max(math.sqrt(math.pow(X[i+1] - X[i], 2) + math.pow(Y[i+1] - Y[i], 2)), eps)
 
-plt.show()
-'''
+min_eps = 200
+for i in range(len(X) - 1):
+    min_eps = min(math.sqrt(math.pow(X[i+1] - X[i], 2) + math.pow(Y[i+1] - Y[i], 2)), min_eps)
+
+mass = [(X, Y)]
+x = 0
+y = 0
+idx = 0
+while (min_eps < eps):
+    new_mass = []
+    for i in range(len(mass)):
+        array_len = len(mass[i][0])
+        if array_len > 2: #если больше дух точек в массиве
+            X_f = []
+            Y_f = []
+            X_s = []
+            Y_s = []
+            half = array_len/2
+            for f in range(array_len):
+                if (f < half):
+                    X_f.append(mass[i][0][f])
+                    Y_f.append(mass[i][1][f])
+                else:
+                    X_s.append(mass[i][0][f])
+                    Y_s.append(mass[i][1][f])
+            pow_x = math.pow(abs(X_f[len(X_f)-1] - X_f[0]), 2)
+            pow_Y = math.pow(abs(Y_f[len(Y_f)-1] - Y_f[0]), 2)
+            swap_x = math.sqrt(pow_x + pow_Y)
+            if (swap_x <= eps):#если расстояние между последними точками меньше eps, то оставляем только последние точки
+                X_new = [X_f[0], X_f[len(X_f)-1]]
+                Y_new = [Y_f[0], Y_f[len(Y_f)-1]]
+                new_mass.append((X_new, Y_new))
+                new_mass.append((X_s, Y_s))
+            else:#иначе делим на 2 новых массива
+                for j in range(len(mass)):
+                    if (j == i):
+                        new_mass.append((X_f, Y_f))
+                        new_mass.append((X_s, Y_s))
+                    else:
+                        new_mass.append(mass[j])
+        else:#иначе не трогаем этот массив
+            new_mass.append(mass[i])
+    mass = new_mass
+    min_eps = eps
+    max_len = 0
+    for e in new_mass:
+        max_len = max(len(e), max_len)
+        X = e[0]
+        Y = e[1]
+        for r in range(len(X) - 1):
+            min_eps = min(math.sqrt(math.pow(X[r+1] - X[r], 2) + math.pow(Y[r+1] - Y[r], 2)), min_eps)
+    if max_len <= 2:
+        break
+mass = []
+for e in new_mass:
+    X = e[0]
+    Y = e[1]
+    for z in range(len(e[0])):
+        mass.append((X[z], Y[z]))
 __author__ = 'lisgein'
