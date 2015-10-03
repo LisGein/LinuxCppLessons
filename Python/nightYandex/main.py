95fd695be9a12ed7033c7d9855bba27821384741
LisGein <alred402@gmail.com>
2015-10-03 15:54:21 +0300

    упорядочила массив с координатами

--- Python/nightYandex/main.py
+++ Python/nightYandex/main.py
@@ -1,113 +1,150 @@
 import pandas
 import math
+import re
+import time
+from time import gmtime, strftime
 
-data = pandas.read_csv('data1.tsv', sep="\t")
-route_to_stops_count = pandas.read_csv('route1.tsv', sep="\t")
-debug = pandas.read_csv('debug1.tsv', sep="\t")
-# names=['date', 'id', 'type', 'hash', 'latitude', 'longitude']
+def del_invalid_idx_X(X):
+    x =[]
+    for i in X:
+        x.append(i)
+    return x
+
+def del_invalid_idx_Y(Y):
+    x =[]
+    for i in Y:
+        x.append(i)
+    return x
 
 
-X = debug["latitude"].copy()
-Y = debug["longitude"].copy()
+def main():
+    data = pandas.read_csv('data1.tsv', sep="\t")
+    route_to_stops_count = pandas.read_csv('route1.tsv', sep="\t")
+    debug = pandas.read_csv('debug1.tsv', sep="\t")
+    # names=['date', 'id', 'type', 'hash', 'latitude', 'longitude']
 
-mass = [(X, Y)]
+    X = debug["latitude"].copy()
+    Y = debug["longitude"].copy()
+    eps = 0
+    q = 0
+    w = 0
+    e = 0
+    for i in range(len(X)):
+        for j in range(len(X)):
+            if i != j:
+                q = max(math.sqrt(math.pow(X[j] - X[i], 0) + math.pow(Y[j] - Y[i], 2)), q)
+                if (q > eps):
+                    w = i
+                    e = j
+    print(w, e)
+    idx = 0
+    new_x = []
+    new_y = []
 
-new_mass_f = []
-for i in range(len(mass)):
-    X = mass[i][0]
-    Y = mass[i][1]
-    for j in range(len(X)):
-        if len(new_mass_f) > 0:
-            if (new_mass_f[len(new_mass_f)-1][0] != X[j]) or (new_mass_f[len(new_mass_f)-1][1]!=Y[j]):
-                new_mass_f.append((X[j],Y[j]))
-            else:
-                print(new_mass_f[len(new_mass_f)-1][0], X[j])
-                print(new_mass_f[len(new_mass_f)-1][1], Y[j])
-        else:
-            new_mass_f.append((X[j],Y[j]))
-new_mass = []
-X = []
-Y = []
-for i in range(len(new_mass_f)):
-    X.append(new_mass_f[i][0])
-    Y.append(new_mass_f[i][1])
+    new_x.append(X.pop(0))
+    new_y.append(Y.pop(0))
+    X = del_invalid_idx_X(X)
+    Y = del_invalid_idx_Y(Y)
 
-mass = [(X, Y)]
+    next_eps = 100
+    while len(X) != 0:
+        for i in range(len(X)):
+            idx = i
+            next_eps = min(math.sqrt(math.pow(X[i] - new_x[len(new_x)-1], 2) + math.pow(Y[i] - new_y[len(new_y)-1], 2)), next_eps)
+        print(new_x[len(new_x)-1], new_y[len(new_y)-1])
+        print(X[idx], Y[idx])
+        print(idx, next_eps, eps)
+        new_x.append(X.pop(idx))
+        new_y.append(Y.pop(idx))
+        X = del_invalid_idx_X(X)
+        Y = del_invalid_idx_Y(Y)
+
+    print(new_x)
+"""
 eps = 0
+idx = 0
 for i in range(len(X) - 1):
+    idx = i
     eps = max(math.sqrt(math.pow(X[i+1] - X[i], 2) + math.pow(Y[i+1] - Y[i], 2)), eps)
+print(idx)
 
-min_eps = 200
-for i in range(len(X) - 1):
-    min_eps = min(math.sqrt(math.pow(X[i+1] - X[i], 2) + math.pow(Y[i+1] - Y[i], 2)), min_eps)
+coor = []
+for i in range(len(c_array[0][0])-3):
+    dist = math.sqrt(math.pow(c_array[0][0][len(c_array[0][0]) - i-3] - c_array[0][0][0], 2)
+                         + math.pow(c_array[0][1][len(c_array[0][0]) - i-3] - c_array[0][1][0], 2))
+    if (dist <= eps):
+        l_array = []
+        r_array = []
+        for f in range(len(c_array[0][0])-3):
+            if f < (len(c_array[0][0]) - i-2):
+                l_array.append((c_array[0][0][f], c_array[0][1][f]))
+            else:
+                r_array.append((c_array[0][0][f], c_array[0][1][f]))
+
+        coor.append(l_array)
+        coor.append(r_array)
+print(coor)
 
 
-while (min_eps < eps):
-    new_mass = []
-    for i in range(len(mass)):
-        array_len = len(mass[i][0])
-        if array_len > 3: #если больше двух точек в массиве
-            X_f = []
-            Y_f = []
-            X_s = []
-            Y_s = []
-            half = array_len/2
-            for f in range(array_len):
-                if (f < half):
-                    X_f.append(mass[i][0][f])
-                    Y_f.append(mass[i][1][f])
-                elif (f == half):
-                    X_f.append(mass[i][0][f])
-                    Y_f.append(mass[i][1][f])
-                    X_s.append(mass[i][0][f])
-                    Y_s.append(mass[i][1][f])
-                else:
-                    X_s.append(mass[i][0][f])
-                    Y_s.append(mass[i][1][f])
-            pow_x = math.pow(abs(X_f[len(X_f)-1] - X_f[0]), 2)
-            pow_Y = math.pow(abs(Y_f[len(Y_f)-1] - Y_f[0]), 2)
-            swap_x = math.sqrt(pow_x + pow_Y)
-            if (swap_x <= eps):#если расстояние между крайними точками меньше eps, то оставляем только последние точки
-                X_new = [X_f[0], X_f[len(X_f)-1]]
-                Y_new = [Y_f[0], Y_f[len(Y_f)-1]]
-                new_mass.append((X_new, Y_new))
-                new_mass.append((X_s, Y_s))
-            else:#иначе делим на 2 новых массива
-                for j in range(len(mass)):
-                    if (j == i):
-                        new_mass.append((X_f, Y_f))
-                        new_mass.append((X_s, Y_s))
-                    else:
-                        new_mass.append(mass[j])
-        else:#иначе не трогаем этот массив
-            new_mass.append(mass[i])
-    max_size = 0
-    for i in range(len(new_mass[0])):
-        for j in range(len(new_mass[i][0])):
-            max_size = max(len(new_mass[i][0]), max_size)
-    if max_size > 2:
-        mass = new_mass
-    else:
-        new_mass_f = []
-        for i in range(len(new_mass)):
-            X = new_mass[i][0]
-            Y = new_mass[i][1]
-            for j in range(len(X)):
-                if len(new_mass_f) > 0:
-                    if (new_mass_f[len(new_mass_f)-1][0] != X[j]) or (new_mass_f[len(new_mass_f)-1][1]!=Y[j]):
-                        new_mass_f.append((X[j],Y[j]))
-                else:
-                    new_mass_f.append((X[j],Y[j]))
-        X = []
-        Y = []
-        for i in range(len(new_mass_f)):
-            X.append(new_mass_f[i][0])
-            Y.append(new_mass_f[i][1])
-        mass = [(X, Y)]
-        print(len(mass[0][0]), "end\n")
-        min_eps = eps
-        break
-print(mass)
 
+
+
+import pandas
+import math
+
+def break_array(c_array, eps):
+    new_coor = []
+    for i in range(len(c_array[0])-1):
+        dist = math.sqrt(math.pow(c_array[0][len(c_array) - i-1] - c_array[0][0], 2)
+                         + math.pow(c_array[1][len(c_array) - i-1] - c_array[1][0], 2))
+        if (dist <= eps):
+            l_array = []
+            r_array = []
+            for f in range(i):
+                l_array.append(c_array[f])
+            for f in range(c_array):
+                r_array.append(c_array[f])
+
+            new_coor.append(l_array)
+            new_coor.append(r_array)
+            return new_coor
+    new_coor = [c_array]
+    return new_coor
+
+
+
+def main():
+    data = pandas.read_csv('data1.tsv', sep="\t")
+    route_to_stops_count = pandas.read_csv('route1.tsv', sep="\t")
+    debug = pandas.read_csv('debug1.tsv', sep="\t")
+    # names=['date', 'id', 'type', 'hash', 'latitude', 'longitude']
+
+
+    X = debug["latitude"].copy()
+    Y = debug["longitude"].copy()
+
+    c_array = []
+    c_array.append((X, Y))
+
+    eps = 0
+    for i in range(len(X) - 1):
+        eps = max(math.sqrt(math.pow(X[i+1] - X[i], 2) + math.pow(Y[i+1] - Y[i], 2)), eps)
+
+    coor = break_array(c_array[0], eps)
+    print(coor)
+    c_array = coor[1]
+    dist = math.sqrt(math.pow(c_array[c_array.size()-1].first - c_array[0].first , 2) + math.pow(c_array[c_array.size()-1].second - c_array[0].second, 2))
+    new_coor = []
+    while (dist > eps):
+        new_coor.push_back(coor[0])
+        coor = break_array(coor[1], eps)
+        c_array = coor[1]
+        dist = math.sqrt(math.pow(c_array[c_array.size()-1].first - c_array[0].first , 2) + math.pow(c_array[c_array.size()-1].second - c_array[0].second, 2))
+
+    print(new_coor)
+
+"""
+if __name__ == "__main__":
+    main()
 
 __author__ = 'lisgein'
--- test_python_alg/main.cpp
+++ test_python_alg/main.cpp
@@ -3,56 +3,86 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <cstdlib>
+#include <algorithm>
 
+typedef std::vector<std::pair<double, double> > array_coor;
+typedef std::vector<std::vector<std::pair<double, double> > > array;
+
+array break_array(array_coor c_array, double eps)
+{
+   array new_coor;
+   for (int i = c_array.size()-1; i > 0; --i)
+   {
+      double dist = std::sqrt(std::pow(c_array[i+1].first - c_array[0].first , 2) + std::pow(c_array[i+1].second - c_array[0].second, 2));
+      if (dist <= eps)
+      {
+         array_coor l_array;
+         array_coor r_array;
+         for (int f = 0; f <= i; ++f)
+            l_array.push_back(c_array[f]);
+
+         for (int f = i; f < c_array.size()-1; ++f)
+            r_array.push_back(c_array[f]);
+
+         new_coor.push_back(l_array);
+         new_coor.push_back(r_array);
+         std::cout << dist << " " << i << " break\n";
+         break;
+      }
+   }
+   for (int i = c_array.size()-1; i > 0; --i)
+   std::cout << std::sqrt(std::pow(c_array[i+1].first - c_array[0].first , 2) + std::pow(c_array[i+1].second - c_array[0].second, 2)) <<"\n";
+   std::cout << eps <<" eps\n";
+   return new_coor;
+}
 
 int main()
 {
-//         std::ifstream data("data1.tsv");
-//         std::string str;
-//         while ( std::getline(data, str))
-//                  std::cout << str << std::endl;
-
-         std::vector< std::vector<std::pair<int, int> > > array;
-         std::vector<std::pair<int, int> > s_array = {std::make_pair<int, int>(1, 2),
-                                                     std::make_pair<int, int>(2, 3),
-                                                     std::make_pair<int, int>(4,8)};
-         array.push_back(s_array);
-         s_array = {std::make_pair<int, int>(4, 8),
-                    std::make_pair<int, int>(6, 3),
-                    std::make_pair<int, int>(1, 8)};
-         array.push_back(s_array);
-         for (int i = 0; i < array.size(); ++i)
-                  for (int j = 0; j < array[i].size(); ++j)
-                           std::cout << array[i][j].first << ", " << array[i][j].second << std::endl;
-
-         std::vector<std::pair<int, int > > mass;
-         for (int i = 0; i < array.size(); ++i)
-         {
-                  std::pair<int, int> prev_pair(array[i][0].first, array[i][0].second);
-                  if (!mass.empty())
-                  {
-                           if (prev_pair != mass[mass.size()-1])
-                                    mass.push_back(prev_pair);
-                  }
-                  else
-                           mass.push_back(prev_pair);
-                  for (int j = 1; j < array[i].size(); ++j)
-                  {
-                           std::pair<int, int> prev_pair(array[i][j].first, array[i][j].second);
-                           if (prev_pair != mass[mass.size()-1])
-                                 mass.push_back(prev_pair);
-
-                  }
-                  std::pair<int, int> next_pair(array[i][array[i].size()-1].first, array[i][array[i].size()-1].second);
-                  if (next_pair != mass[mass.size()-1])
-                           mass.push_back(next_pair);
-         }
+   std::ifstream data_lat("lat.tsv");
+   std::string name;
+   char lat [256];
+   data_lat >> name;
+   std::vector<double>  lat_coor;
+   while (data_lat >> lat)
+      lat_coor.push_back(std::atof(lat));
+   data_lat.close();
 
 
-         std::vector< std::vector<std::pair<int, int> > > new_array = {mass};
+   std::ifstream data_lon("lat.tsv");
+   char lon [256];
+   data_lon >> name;
+   std::vector<double>  lon_coor;
+   while (data_lon >> lon)
+      lon_coor.push_back(std::atof(lat));
+   data_lon.close();
 
 
+   array_coor c_array;
+   double eps = 0;
+   for (int i = 0; i < lon_coor.size();++i)
+   {
+      c_array.push_back(std::make_pair(lat_coor[i], lon_coor[i]));
+      if (i != lon_coor.size()-1)
+         eps = std::max(std::sqrt(std::pow(lat_coor[i+1] - lat_coor[i], 2) + std::pow(lon_coor[i+1] - lon_coor[i], 2)), eps);
+      std::cout << lat_coor[i] << " " << lon_coor[i] << " \n";
+   }
 
-         return 0;
+   array coor = break_array(c_array, eps);
+   c_array = coor[1];
+   double dist = std::sqrt(std::pow(c_array[c_array.size()-1].first - c_array[0].first , 2) + std::pow(c_array[c_array.size()-1].second - c_array[0].second, 2));
+
+
+   array new_coor;
+   while (dist>eps)
+   {
+      new_coor.push_back(coor[1]);
+      coor = break_array(coor[0], eps);
+
+      c_array = coor[1];
+      dist = std::sqrt(std::pow(c_array[c_array.size()-1].first - c_array[0].first , 2) + std::pow(c_array[c_array.size()-1].second - c_array[0].second, 2));
+   }
+
+   return 0;
 }
 
