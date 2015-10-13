28a7c146fe4212a902957f7a096b5392af3bd963
LisGein <alred402@gmail.com>
2015-10-13 05:00:26 +0300

    Окончательно доломанный алгоритм

--- Python/nightYandex/sort_coordinates.py
+++ Python/nightYandex/sort_coordinates.py
@@ -28,4 +28,53 @@ def sort_coordinates(x, y):
     array_coordinates = [new_x, new_y]
     return array_coordinates
 
+
+def len_way(x1, y1, x2, y2):
+    return math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2)
+
+
+def div_way(x, y):
+    ways_x = []
+    ways_y = []
+
+    max_eps = 0.005
+    f_point = 0
+    for j in range(len(x) - 6):
+        idx = f_point+j+2
+        if idx + 3 >= len(x):
+            idx = len(x)
+            print(idx, len(x))
+            ways_x.append(x[f_point:idx])
+            ways_y.append(y[f_point:idx])
+            break
+        else:
+            way_f = len_way(x[0], y[0], x[idx], y[idx])
+            idx += 1
+            way_s = len_way(x[0], y[0], x[idx], y[idx])
+            idx += 1
+            way_t = len_way(x[0], y[0], x[idx], y[idx])
+            if way_f > max_eps and way_s > max_eps and way_t > max_eps:
+                while 1:
+                    idx += 1
+                    if idx == len(x):
+                        idx -= 1
+                        break
+                    way_next = len_way(x[0], y[0], x[idx], y[idx])
+                    if way_next < max_eps:
+                        idx += 1
+                        if idx == len(x):
+                            idx -= 1
+
+                        break
+
+                idx += 1
+                ways_x.append(x[f_point:idx])
+                ways_y.append(y[f_point:idx])
+                f_point = idx
+                print(idx, len(x))
+                if f_point + j + 6 >= len(x):
+                    break
+    return [ways_x, ways_y]
+
+
 __author__ = 'lisgein'
