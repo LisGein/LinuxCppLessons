d3ef510742bab02bfab40f27aa5fdfc6272f7581
lisgein <alred402@gmail.com>
2015-10-28 15:17:06 +0300

    Рассчёт скорости

--- Python/nightYandex/speed.py
+++ Python/nightYandex/speed.py
@@ -37,4 +37,27 @@ def sort_coor(lat_points, lon_points, speed):
         sort_lon.append(lon)
         sort_speed.append(sp)
 
+
+def mean_speed(speed, mean_stays_rounds):
+    mean_speeds = 0
+    for i in speed:
+        mean_speed = 0
+        for j in i:
+            mean_speed += j
+        mean_speeds += mean_speed/len(i)
+    mean_speeds /= len(speed)
+    eps = (mean_speeds/2)
+
+    mean_speeds_rounds = []
+    for i in range(len(speed) - 1):
+        round_mean = []
+        for j in range(len(speed[i]) - 1):
+            if mean_speeds - eps < speed[i][j] < mean_speeds + eps:
+                round_mean.append((mean_stays_rounds[i][j], mean_stays_rounds[i][j+1], speed[i][j]))
+        if len(round_mean) > 0:
+            mean_speeds_rounds.append(round_mean)
+
+    return mean_speeds_rounds
+
+
 __author__ = 'lisgein'
