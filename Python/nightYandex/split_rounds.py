79e499600398edc67159c93d8c4ef62f4795ddee
LisGein <alred402@gmail.com>
2015-11-05 05:51:56 +0300

    Функции подсчёта длины

--- Python/nightYandex/split_rounds.py
+++ Python/nightYandex/split_rounds.py
@@ -64,9 +64,27 @@ def mean_stay(rounds):
         mean_stays += (len(i))
     mean_stays /= len(rounds)
     eps = int(mean_stays/2)
-    
+
     mean_stays_rounds = []
     for i in rounds:
         if mean_stays - eps < len(i) < mean_stays + eps:
             mean_stays_rounds.append(i)
     return mean_stays_rounds
+
+
+def distance_line(point, line):
+    a = [line[0][0] - point[0], line[0][1] - point[1]]
+    b = [line[1][0] - point[0], line[1][1] - point[1]]
+    return distance(a, b)/distance([point[0], line[0][0]], [point[1], line[0][1]])
+
+
+def distance_way(start_point, lines):
+    len_way = 0
+    ways = []
+    for i in lines:
+        len_way += distance(start_point, i[0])
+        ways.append([len_way, i])
+        len_way += distance(i[0], i[1])
+        start_point = i[1]
+
+    return ways
